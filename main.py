6a8766aa446e5ab38da1feec3654165d43888402
Biel Camprubí <bielcamprubi@gmail.com>
2023-05-03 20:03:44 +0200

    Added debug mode

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from src.controller import controller
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-c = controller()
-c.run()
+if os.getenv("DEBUG") == "1":
+    import time
+
+    while True:
+        time.sleep(10)
+
+else:
+    c = controller()
+    c.run()
